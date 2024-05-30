f0b7b82cf572c71710ecb4dce11f41e3526ee646
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-05-30 13:41:03 +0400

    cleanup

--- flaskapplication/website/__init__.py
+++ flaskapplication/website/__init__.py
@@ -5,12 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+
 def create_app():
-    #Creating new App
+    # Creating new App
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "Simple Secret Key"
 
-    # # Adding database to the application
+    # Adding database to the application
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -20,10 +21,11 @@ def create_app():
 
     # Create Database
     create_database(app)
+
     return app
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
         with app.app_context():
             db.create_all()
-        print("Database Created")
+            print("Database Created")
--- flaskapplication/website/routes.py
+++ flaskapplication/website/routes.py
@@ -11,25 +11,27 @@ routes = Blueprint('routes', __name__)
 def home():
     return "Welcome to First Flask Project!"
 @routes.route('/loaddata')
-def loaddata():
+def loaddata(): 
     data = Course.query.all()
     print(len(data))
-    if(len(data) <5):
-        # opening the json file 
+    if len(data) < 5:
+        # Opening JSON file
         f = open('courses.json')
 
-        #load json as dictionary
+        # Load JSON object as an dictionary
         courses = json.load(f)
-        #add course to database
+        
+        # Adding all courses to the database
         for course in courses:
-            new_course = Course(title=course['title'],author = course['author'],overview = course['overview'],image = course['img'],url = course['url'])
-            #adding course to database
+            # Creating a new Course
+            new_course = Course(title=course['title'], author=course['author'], overview=course['overview'], image=course['img'], url=course['url'])
+            # Adding course to database
             db.session.add(new_course)
             db.session.commit()
+        # Closing File
         f.close()
-    return "Data Loaded Succesfully"
-
-
+        # Success Message
+    return "Data Loaded Successfully"
 
 
         
