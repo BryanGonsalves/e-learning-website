94ff732f4fcbc1a2c291aa7d7d362011d29fcea8
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-05-30 13:13:28 +0400

    Added Course Model

---
+++ flaskapplication/website/models.py
@@ -0,0 +1,13 @@
+from . import db
+
+# first model
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20))
+    author = db.Column(db.String(50))
+    overview = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+    url = db.Column(db.String(100))
+
+    
