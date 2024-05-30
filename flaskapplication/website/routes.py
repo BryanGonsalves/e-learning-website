61770b75e9e79deb3315b966bc63f6e4a1525977
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-05-30 13:30:35 +0400

    courses into database

--- flaskapplication/website/routes.py
+++ flaskapplication/website/routes.py
@@ -1,8 +1,35 @@
-from flask import Blueprint, render_template
-from flask import request
+import json
+
+from flask import Blueprint, render_template, request
+
+from . import db
+from .models import Course
 
 routes = Blueprint('routes', __name__)
 
 @routes.route('/')
 def home():
     return "Welcome to First Flask Project!"
+@routes.route('/loaddata')
+def loaddata():
+    data = Course.query.all()
+    print(len(data))
+    if(len(data) <5):
+        # opening the json file 
+        f = open('courses.json')
+
+        #load json as dictionary
+        courses = json.load(f)
+        #add course to database
+        for course in courses:
+            new_course = Course(title=course['title'],author = course['author'],overview = course['overview'],image = course['img'],url = course['url'])
+            #adding course to database
+            db.session.add(new_course)
+            db.session.commit()
+        f.close()
+    return "Data Loaded Succesfully"
+
+
+
+
+        
